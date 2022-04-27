b998a9d9de364051c4f24e7f7ae6e74ec5205cdf
Bradford Wagner <bwagner@blackrock.com>
2022-04-27 11:52:40 -0400

    test scoping

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/sirupsen/logrus"
+import (
+	"errors"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
 	l := logrus.New()
-	l.Info("hello world")
+
+	a := "a"
+	l.Info("a: ", a)
+
+	a, err := "b", errors.New("test error")
+	l.WithError(err).Info("a: ", a)
+
+	var f = func() {
+		b, err := "1234", errors.New("func error")
+		l.WithError(err).Info("b: ", b)
+	}
+	f()
+	l.WithError(err).Info("a: ", a) // note the error set by func isn't in scope
+
 }
