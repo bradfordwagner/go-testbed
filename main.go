602526037321cefdb25dd5dd664a48e39d9ba45a
Bradford Wagner <bwagner@blackrock.com>
2022-05-12 14:51:59 -0400

    update

--- main.go
+++ main.go
@@ -1,24 +1,18 @@
 package main
 
-import (
-	"errors"
-	"github.com/sirupsen/logrus"
-)
+import "github.com/sirupsen/logrus"
 
 func main() {
 	l := logrus.New()
 
-	a := "a"
-	l.Info("a: ", a)
-
-	a, err := "b", errors.New("test error")
-	l.WithError(err).Info("a: ", a)
-
-	var f = func() {
-		b, err := "1234", errors.New("func error")
-		l.WithError(err).Info("b: ", b)
+	m := map[string]string{
+		"a": "a",
 	}
-	f()
-	l.WithError(err).Info("a: ", a) // note the error set by func isn't in scope
+
+	l.Info("map: ", m)
+
+	delete(m, "b")
+
+	l.Info("map: ", m)
 
 }
